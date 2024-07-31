3d70a70194c38e28c4b66156b8ca6383f113cfb3
tulio-limaa <tulio.lima310@gmail.com>
2024-07-31 10:38:17 -0400

    Add files via upload

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.Data;
+using StudentManagement.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
